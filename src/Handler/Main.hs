8d5d0823786f44a2e57ae46135a3c737186e77ad
Dmitry Suzdalev <dimsuz@gmail.com>
2017-07-22 12:40:16 +0200

    Add a Main handler

---
+++ src/Handler/Main.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Main where
+
+import Import
+
+getMainR :: Handler Html
+getMainR = do
+  defaultLayout $ do
+    $(widgetFile "mainpage")
