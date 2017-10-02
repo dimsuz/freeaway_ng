1ccc1551a66c6fff4bb783bcaee9ab7ca6f26698
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-03 00:08:48 +0200

    Extract file names

--- scripts/src/GenImagesImport.hs
+++ scripts/src/GenImagesImport.hs
@@ -3,11 +3,24 @@ module Main where
 
 import Turtle
 import Prelude hiding (FilePath)
+import Filesystem.Path.CurrentOS (encodeString)
+
+data ImageMetaData = ImageMetaData
+  {
+    fileName :: String
+  , width :: Int
+  , height :: Int
+  }
+  deriving Show
 
 parser :: Parser FilePath
 parser = argPath "SRC_DIR"  "A source directory to find an images to import"
 
+extractImageData :: FilePath -> ImageMetaData
+extractImageData fp = ImageMetaData (encodeString fp) 0 0
+
 main :: IO ()
 main = do
   sourceDir <- options "Generates an .sql file to use for importing images meta info into DB" parser
+  files <- view (fmap extractImageData (ls sourceDir))
   printf ("Got directory "%fp%"\n") sourceDir
