f4a795c388bfc79e247d67c933f3b4c5ee9aaa94
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-17 21:46:31 +0200

    Implement executing 'file' on image files

--- scripts/src/GenImagesImport.hs
+++ scripts/src/GenImagesImport.hs
@@ -4,10 +4,12 @@ module Main where
 import Turtle
 import Prelude hiding (FilePath)
 import Filesystem.Path.CurrentOS (encodeString)
+import qualified Control.Foldl as Fold
+import qualified Data.Text as T
 
 data ImageMetaData = ImageMetaData
   {
-    fileName :: String
+    fileName :: Text
   , width :: Int
   , height :: Int
   }
@@ -17,10 +19,16 @@ parser :: Parser FilePath
 parser = argPath "SRC_DIR"  "A source directory to find an images to import"
 
 extractImageData :: FilePath -> ImageMetaData
-extractImageData fp = ImageMetaData (encodeString fp) 0 0
+extractImageData fp = ImageMetaData (T.pack $ encodeString fp) 0 0
+
+extract :: Text -> IO (Maybe Line)
+extract t = single (inproc "file" [t] empty)
 
 main :: IO ()
 main = do
   sourceDir <- options "Generates an .sql file to use for importing images meta info into DB" parser
   files <- view (fmap extractImageData (ls sourceDir))
-  printf ("Got directory "%fp%"\n") sourceDir
+  fileNames <- fold (fmap (fileName . extractImageData)  (ls sourceDir)) Fold.list
+  -- printf ("Got directory "%fp%"\n") sourceDir
+  output <- mapM extract $ take 3 fileNames
+  print output
