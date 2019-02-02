478417208b3684ed31be6000a9de152e6d0b3996
Dmitry Suzdalev <dimsuz@gmail.com>
2019-02-02 17:34:43 +0200

    Implement seeding expressions from expressions.yml

--- test/Seed.hs
+++ test/Seed.hs
@@ -4,23 +4,32 @@ module Main where
 
 import Import
 import Model
+import qualified Control.Exception as Exception
 import Yesod.Default.Config2 (useEnv, loadYamlSettings)
 import Database.Persist.Sqlite              (sqlDatabase, wrapConnection, createSqlPool, runSqlPool)
 import qualified Database.Sqlite as Sqlite
 import Control.Monad.Logger (runStderrLoggingT)
+import Data.Yaml                   (decodeFileEither, ParseException, withText)
 
+instance FromJSON Expression where
+  parseJSON = withText "Expression" $ \t -> return (Expression t)
 
 rawConnection :: Text -> IO Sqlite.Connection
 rawConnection = Sqlite.open
 
-insertExpression :: MonadIO m => Text -> ReaderT SqlBackend m ()
-insertExpression expr = insert_ $ Expression expr
+loadExpressions :: MonadIO m => FilePath -> m [Expression]
+loadExpressions filepath = do
+  decodeResult <- liftIO $ decodeFileEither filepath
+  return $ either Exception.throw id decodeResult
+
+insertExpression :: MonadIO m => Expression -> ReaderT SqlBackend m ()
+insertExpression expr = insert_ expr
 
 seed :: MonadIO m => ReaderT SqlBackend m ()
 seed = do
   deleteWhere ([] :: [Filter Expression])
-  insertExpression "hello"
-  return ()
+  expressions <- loadExpressions "config/expressions.yml"
+  sequence_ $ map insert_ expressions
 
 main :: IO ()
 main = do
