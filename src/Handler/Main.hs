5f4657c7f935fbf30f8ecf5fc88a21625ce84aa0
Dmitry Suzdalev <dimsuz@gmail.com>
2019-02-23 19:00:32 +0200

    Implement random image selection from DB

--- src/Handler/Main.hs
+++ src/Handler/Main.hs
@@ -6,15 +6,15 @@
 module Handler.Main where
 
 import Import
+import qualified Data.List as L ((!!))
+import System.Random (randomRIO)
 
 getMainR :: Handler Html
 getMainR = do
+  -- TODO selectKeys, take list length, select random key, then use selectFirst
   imageCount <- runDB $ count ([] :: [Filter ExpressionImage])
+  imageIndex <- liftIO $ randomRIO (0, imageCount - 1)
+  images <- runDB $ selectList ([] :: [Filter ExpressionImage]) []
+  let (Entity _ image) = images L.!! imageIndex
   exprCount <- runDB $ count ([] :: [Filter Expression])
-  defaultLayout $ do
-    $(widgetFile "mainpage_new")
-
-getRandomImage :: App -> FilePath
-getRandomImage master =
-  let staticDir = appStaticDir $ appSettings master
-  in staticDir </> "img" </> "gallery"
+  defaultLayout $(widgetFile "mainpage_new")
